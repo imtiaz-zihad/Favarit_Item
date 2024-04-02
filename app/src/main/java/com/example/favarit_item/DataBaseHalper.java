2e7137183a3c5a3a47c2e34aa17c2e5cabbe9652
Md Soriful hasan <sorifullhasan300@gmail.com>
2024-04-02 13:56:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/favarit_item/DataBaseHalper.java
@@ -0,0 +1,94 @@
+package com.example.favarit_item;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHalper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "users";
+    public static final int DB_VERSION = 1;
+    public static final String DB_TABLE_NAME = "users_table";
+    Context context;
+
+
+    public DataBaseHalper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context =context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(" CREATE TABLE " + DB_TABLE_NAME + " (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,isFavorite INTEGER) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + DB_TABLE_NAME );
+    }
+
+
+    public boolean InsertData (String Name){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues conVal = new ContentValues();
+        conVal.put("name",Name);
+        conVal.put("isFavorite",0);
+        long result = database.insert(DB_TABLE_NAME,null,conVal);
+
+        if (result <= 0){
+           return false;
+        }else {
+            return true;
+        }
+    } // insert data end tag
+
+
+    public Cursor getUserData () {
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery("select * from " + DB_TABLE_NAME, null);
+        if (cursor.getCount() != 0) {
+            return cursor;
+        }
+            return null;
+    }//getUserData end tag
+
+
+    public void updateAddFavorite (int id){
+        SQLiteDatabase database = this.getWritableDatabase();
+        try {
+            int value = 1;
+            database.execSQL("UPDATE "+DB_TABLE_NAME+ " SET isFavorite= '" +value+ "' WHERE id = "+id);
+            Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show();
+
+        }catch (SQLException e){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }
+
+
+}// updateAddFavorite end tag
+
+    public void updateRemoveFavorite (int id){
+
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        try {
+            int value=0;
+            database.execSQL("UPDATE "+DB_TABLE_NAME+" SET isFavorite'" +value+ "' WHERE id ="+id);
+            Toast.makeText(context, "UnFavourite", Toast.LENGTH_SHORT).show();
+        }catch (SQLException e){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
+
+
+
+} //end tag
---
+++ app/src/main/java/com/example/favarit_item/MainActivity.java
@@ -0,0 +1,211 @@
+package com.example.favarit_item;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    DataBaseHalper dataBaseHalper;
+    ArrayList <HashMap <String,String>> arrayList = new ArrayList<>();
+    HashMap <String,String> hashMap;
+    Adapter myAdapter;
+    FloatingActionButton floatingButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        dataBaseHalper = new DataBaseHalper(this);
+        floatingButton = findViewById(R.id.floatingButton);
+
+        listView = findViewById(R.id.listview);
+
+       getData();
+
+       floatingButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               showDialogBox ();
+               getData();
+           }
+       });
+
+      myAdapter = new MyAdapter();
+      listView.setAdapter((ListAdapter) myAdapter);
+
+      
+
+    }
+    public class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater layoutInflater = getLayoutInflater();
+            View view = layoutInflater.inflate(R.layout.itemlist,parent,false) ;
+            TextView tvName;
+            ImageView favoriteImage;
+
+            tvName=view.findViewById(R.id.tvName);
+            favoriteImage=view.findViewById(R.id.favoriteImage);
+
+            HashMap <String,String> myhashmap =arrayList.get(position);
+
+              String id = myhashmap.get("id");
+              String name = myhashmap.get("name");
+              String isFavorite = myhashmap.get("isFavorite");
+
+
+              tvName.setText(name);
+
+              if (isFavorite.equals(0)){
+
+                  favoriteImage.setImageResource(R.drawable.unfill_favorite);
+              }else {
+
+                  favoriteImage.setImageResource(R.drawable.fill_favorite);
+              }
+
+          favoriteImage.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+                  if (isFavorite.equals(0)){
+
+                      dataBaseHalper.updateAddFavorite(Integer.parseInt(id));
+                      favoriteImage.setImageResource(R.drawable.fill_favorite);
+                      getData();
+                    //  notifyDataSetChanged();
+
+                  }else {
+                      dataBaseHalper.updateRemoveFavorite(Integer.parseInt(id));
+                      favoriteImage.setImageResource(R.drawable.unfill_favorite);
+                      getData();
+                  //    notifyDataSetChanged();
+
+                  }
+
+              }
+          });
+
+
+            return view;
+        }
+    } //end adapter
+
+    private void showDialogBox () {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        View myview =getLayoutInflater().inflate(R.layout.input_layout,null) ;
+        alert.setView(myview);
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.setCancelable(true);
+        EditText edName;
+        Button button;
+        button = myview.findViewById(R.id.button);
+        edName = myview.findViewById(R.id.edName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edName.getText().toString();
+                if (name.isEmpty()){
+
+                    Toast.makeText(MainActivity.this, "Plz enter your name", Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    boolean isDataInserted = dataBaseHalper.InsertData(name);
+                    if (isDataInserted){
+                        Toast.makeText(MainActivity.this, "data inserted", Toast.LENGTH_SHORT).show();
+                        getData();
+                     //   myAdapter.notifyAll();
+                        alertDialog.dismiss();
+
+                    }else {
+                        Toast.makeText(MainActivity.this, "data is not inserted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }); //button tag end here
+        alertDialog.show();
+    }//showDialogBox end tag
+
+
+    private void getData (){
+
+        Cursor cursor = dataBaseHalper.getUserData();
+        arrayList.clear();
+        if (cursor!=null){
+
+            while (cursor.moveToNext()){
+
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int isFavorite = cursor.getInt(2);
+
+
+                    hashMap = new HashMap<>();
+                    hashMap.put("id",""+id);
+                    hashMap.put("name",""+name);
+                    hashMap.put("isFavorite",""+isFavorite);
+                    arrayList.add(hashMap);
+
+            }
+        }
+
+    }//getData end here
+
+
+    @Override
+    protected void onResume() {
+
+        getData();
+        super.onResume();
+
+    }
+
+
+
+
+}
+
+
